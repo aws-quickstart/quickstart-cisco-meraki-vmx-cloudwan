c8244087039de7a5ad67a805f0ae5b990f61aa61
Simarbir Singh <1707613+simarbir@users.noreply.github.com>
2022-04-27 11:38:18 -0700

    Remove asn-range ref from lambda template and polling lambda

--- functions/source/meraki/meraki_polling_lambda.py
+++ functions/source/meraki/meraki_polling_lambda.py
@@ -182,11 +182,9 @@ def get_ec2_instance_id(instance_tag):
     else:
         return instance_id[0]
 
-def update_network_event_json(vpn_routes, vpc_arn, subnet_arns, asn_range, global_network_name, event_bus_name, base_region_name):
+def update_network_event_json(vpn_routes, vpc_arn, subnet_arns, global_network_name, event_bus_name, base_region_name):
     network_name = global_network_name
     region = os.environ['AWS_REGION']
-    input_data = json.dumps({"network_name": network_name, "regions": [region], "asn-range": [asn_range], "destination_cidr_blocks": [vpn_routes], "VpcArn": vpc_arn, "SubnetArns": [subnet_arns]}),
-    logger.info('input_data')
     aws_events_client = boto3.client('events', region_name=base_region_name)
     aws_nm_client = boto3.client('networkmanager')
     network = {}
@@ -248,7 +246,7 @@ def update_network_event_json(vpn_routes, vpc_arn, subnet_arns, asn_range, globa
                 #response = requests.put(event['ResourceProperties']['WaitHandle'], data=requests_data, headers={'Content-Type':''})
                 #print (response)
 
-def update_rt(org_id, vmx1_tag, vmx2_tag, vpc_arn, az1_subnet_arn, az2_subnet_arn, rt_id, asn_range, global_network_name, event_bus_name, base_region_name):
+def update_rt(org_id, vmx1_tag, vmx2_tag, vpc_arn, az1_subnet_arn, az2_subnet_arn, rt_id, global_network_name, event_bus_name, base_region_name):
     org_id = org_id
     vmx1_tag = vmx1_tag
     vmx2_tag = vmx2_tag
@@ -294,7 +292,7 @@ def update_rt(org_id, vmx1_tag, vmx2_tag, vpc_arn, az1_subnet_arn, az2_subnet_ar
         logger.info ('vMX1 and vMX2 are BOTH offline')
         #TODO: Cloudwatch enhancement to generate alerts when both vMXs are offline
     logger.info('Running Update Network Event') 
-    update_network_event_json(vpn_routes, vpc_arn, subnet_arns, asn_range, global_network_name, event_bus_name, base_region_name)
+    update_network_event_json(vpn_routes, vpc_arn, subnet_arns, global_network_name, event_bus_name, base_region_name)
 def timeout(event, context):
     logging.error('Execution is about to time out, sending failure response to CloudFormation')
     cfnresponse.send(event, context, cfnresponse.FAILED, {}, None)
@@ -313,11 +311,10 @@ def main(event, context):
         vpc_arn = os.environ['vpc_arn']
         az1_subnet_arn = os.environ['az1_subnet_arn']
         az2_subnet_arn = os.environ['az2_subnet_arn']
-        asn_range = os.environ['asn_range']
         global_network_name = os.environ['global_network_name']
         event_bus_name = os.environ['event_bus_name']
         base_region_name = os.environ['base_region_name']
-        update_rt(org_id, vmx1_tag, vmx2_tag, vpc_arn, az1_subnet_arn, az2_subnet_arn, rt_id, asn_range, global_network_name, event_bus_name, base_region_name)
+        update_rt(org_id, vmx1_tag, vmx2_tag, vpc_arn, az1_subnet_arn, az2_subnet_arn, rt_id, global_network_name, event_bus_name, base_region_name)
 
     except Exception as e:
         logging.error('Exception: %s' % e, exc_info=True)
