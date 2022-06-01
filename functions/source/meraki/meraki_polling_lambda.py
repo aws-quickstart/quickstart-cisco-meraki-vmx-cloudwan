7eb05dca0762ad9559d47eaafdc2eb5d130fb0bf
Simarbir Singh <1707613+simarbir@users.noreply.github.com>
2022-05-31 19:48:37 -0700

    Fix up for polling lambda

--- functions/source/meraki/meraki_polling_lambda.py
+++ functions/source/meraki/meraki_polling_lambda.py
@@ -190,7 +190,7 @@ def update_network_event_json(vpn_routes, vpc_arn, subnet_arns, global_network_n
     network = {}
     cw_static_routes = []
     vpn_routes_flat_list = [routes for sublist in vpn_routes for routes in sublist]
-    str_vpn_routes = ",".join(vpn_routes_flat_list)
+    str_vpn_routes = ",".join(set(vpn_routes_flat_list))
     try: 
         response = aws_nm_client.describe_global_networks()
         for gn in response['GlobalNetworks']:
@@ -222,26 +222,44 @@ def update_network_event_json(vpn_routes, vpc_arn, subnet_arns, global_network_n
                         vpc_attachment_id = attachment[1][0]['AttachmentId']
                         core_network_id = attachment[1][0]['CoreNetworkId']
         #Get routes from cloudwan
-        cw_routes = aws_events_client.get_network_routes(GlobalNetworkId=network['GlobalNetworkId'], RouteTableIdentifier={'CoreNetworkSegmentEdge': {'CoreNetworkId': core_network_id, 'SegmentName': 'sdwan', 'EdgeLocation': region}}))
+        cw_routes = aws_nm_client.get_network_routes(GlobalNetworkId=network['GlobalNetworkId'], RouteTableIdentifier={'CoreNetworkSegmentEdge': {'CoreNetworkId': core_network_id, 'SegmentName': 'sdwan', 'EdgeLocation': region}})
+        print("Routes")
+        print(cw_routes)
         for routes in cw_routes['NetworkRoutes']:
             if routes['Type'] == 'STATIC':
-                cw_static_routes.append(routes)
+                cw_static_routes.append(routes['DestinationCidrBlock'])
             else:
                 logger.info('No static routes in cloudwan core network')
+        print (cw_static_routes)
         if set(str_vpn_routes) != set(cw_static_routes):
-        # new routes or delete old routes
-            response = aws_events_client.put_events(
-                Entries=[
-                {
-                    'Source': 'com.aws.merakicloudwanquickstart',
-                    'DetailType': 'update global network requested',
-                    'Detail': json.dumps({"network_name": network_name, "regions": [region], "destination_cidr_blocks": set(str_vpn_routes), "VpcAttachmentId": [vpc_attachment_id], "CoreNetworkId": core_network_id}),
-                    'EventBusName': event_bus_name
-                }
-                ]
-            )
-            logger.info(response)
-            return response
+            if set(str_vpn_routes):
+            # new routes or delete old routes
+                response = aws_events_client.put_events(
+                    Entries=[
+                    {
+                        'Source': 'com.aws.merakicloudwanquickstart',
+                        'DetailType': 'update global network requested',
+                        'Detail': json.dumps({"network_name": network_name, "regions": [region], "destination_cidr_blocks": [str_vpn_routes], "VpcAttachmentId": [vpc_attachment_id], "CoreNetworkId": core_network_id}),
+                        'EventBusName': event_bus_name
+                    }
+                    ]
+                )
+                logger.info(response)
+                return response
+            else:
+                print('Empty Meraki Routes')
+                response = aws_events_client.put_events(
+                    Entries=[
+                    {
+                        'Source': 'com.aws.merakicloudwanquickstart',
+                        'DetailType': 'update global network requested',
+                        'Detail': json.dumps({"network_name": network_name, "regions": [region], "destination_cidr_blocks": [], "VpcAttachmentId": [vpc_attachment_id], "CoreNetworkId": core_network_id}),
+                        'EventBusName': event_bus_name
+                    }
+                    ]
+                )
+                logger.info(response)
+                return response 
         else:
             logger.info('No new routes, skipping the update state machine')
     except Exception as e:
